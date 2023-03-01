c9777e82b5ec0bbec7909f7a5702b1ead16f045e
Harshavardhan Naidu Gangavarapu <44733600+HarshaVardhanNaiduGangavarapu@users.noreply.github.com>
2023-03-01 16:28:38 -0500

    Added Src folder

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,18 @@
+import {Fragment} from "react";
+import mealsImage from '../../assets/meals.jpg';
+import classes from '../Layout/Header.module.css';
+import HeaderCartButton from "./HeaderCartButton";
+
+const Header = props =>{
+return <Fragment>
+    <header className={classes.header}>
+    <h1>Harsha Eat's</h1>
+    <HeaderCartButton/>
+    </header>
+    <div className={classes['main-image']}>
+    <img src={mealsImage} alt="Yummy food on table"/>
+    </div>
+</Fragment>
+}
+
+export default Header;
